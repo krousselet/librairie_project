4919806a348b5abcf0838c1b42b0c252fbe1ce7f
Kevin R <krous74@gmail.com>
2023-10-09 15:06:13 +0100

    test

---
+++ src/Entity/Categories.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoriesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoriesRepository::class)]
+class Categories
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'categories', cascade: ['persist', 'remove'])]
+    private ?Livres $id_livre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomCategorie = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdLivre(): ?Livres
+    {
+        return $this->id_livre;
+    }
+
+    public function setIdLivre(?Livres $id_livre): static
+    {
+        $this->id_livre = $id_livre;
+
+        return $this;
+    }
+
+    public function getNomCategorie(): ?string
+    {
+        return $this->nomCategorie;
+    }
+
+    public function setNomCategorie(string $nomCategorie): static
+    {
+        $this->nomCategorie = $nomCategorie;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Notifications.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NotificationsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NotificationsRepository::class)]
+class Notifications
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'notifications', cascade: ['persist', 'remove'])]
+    private ?User $id_utilisateur = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $contenu = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdUtilisateur(): ?User
+    {
+        return $this->id_utilisateur;
+    }
+
+    public function setIdUtilisateur(?User $id_utilisateur): static
+    {
+        $this->id_utilisateur = $id_utilisateur;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): static
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
