09a2f66d1091189412afc4e8b81851235dcd4d1f
Kevin R <krous74@gmail.com>
2023-10-09 12:35:20 +0100

    crud made and voters initialized

---
+++ src/Security/Voter/ActionVoter.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ActionVoter extends Voter
+{
+    public const EDIT = 'POST_EDIT'; // ça, c'est pour l'édition des infos
+    public const VIEW = 'POST_VIEW'; // ça, c'est pour le visionnage des infos
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        // replace with your own logic
+        // https://symfony.com/doc/current/security/voters.html
+        return in_array($attribute, [self::EDIT, self::VIEW])
+            && $subject instanceof \App\Entity\User;
+        // ça, ça vérifie qu'il y a bien un utilisateur de créé, et donc de connecté par la variable de session
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        // if the user is anonymous, do not grant access
+        // si l'utilisateur est anonyme (invité), ne pas autoriser l'accès
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        // ... (check conditions and return true to grant permission) ...
+        switch ($attribute) {
+            case self::EDIT:
+                // logic to determine if the user can EDIT
+                // return true or false
+                break;
+            case self::VIEW:
+                // logic to determine if the user can VIEW
+                // return true or false
+                break;
+        }
+
+        return false;
+    }
+}
