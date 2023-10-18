91518522fa14e6b7ec2bc20eb2fb08b7de56a999
Bryan <vernotte.bryan@gmail.com>
2023-10-18 12:41:13 +0100

    configuration mail

---
+++ src/MessageHandler/EnvoiEmailRemerciement.php
@@ -0,0 +1,35 @@
+<?php
+
+    namespace App\MessageHandler;
+
+
+    use App\Message\Commande;
+    use Symfony\Component\Mailer\MailerInterface;
+    use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+
+    #[AsMessageHandler()]
+
+    class EnvoiEmailRemerciement
+    {
+        private $mailer;
+
+        public function __construct(MailerInterface $mailer)
+        {
+            $this->mailer = $mailer;
+        }
+
+        public function __invoke(Commande $message)
+        {
+            $livre = $message->getLivre();
+            $emailUtilisateur = $message->getEmail();
+
+            $email = (new Email())
+                ->from('votre_email@example.com')
+                ->to($emailUtilisateur)
+                ->subject('Merci pour votre commande de ' . $livre)
+                ->text('Nous vous remercions pour votre commande du livre ' . $livre . '.');
+
+            $this->mailer->send($email);
+        }
+    }
