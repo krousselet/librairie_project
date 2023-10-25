6b48145c4a44d222cfe6140497e56cf3ad6b6c5a
Kevin R <krous74@gmail.com>
2023-10-25 16:28:56 +0100

    pre push loris

--- src/Domain/Exemplaires/Dto/ExemplairesDto.php
+++ src/Domain/Exemplaires/Dto/ExemplairesDto.php
@@ -2,20 +2,16 @@
 
 namespace App\Domain\Exemplaires\Dto;
 
-use phpDocumentor\Reflection\Types\Boolean;
-use phpDocumentor\Reflection\Types\Integer;
 use App\Domain\Exemplaires\Exemplaires;
 
 class ExemplairesDto
 {
 
         public function __construct(
-                        public Integer $quantite,
-                        public Boolean $etat,
-                        public string $livre,
-                        public string $emprunt,
-                        public Boolean $statut,
-                        public Integer $IdLivre,
+                        public int $quantite,
+                        public array $etat,
+                        public bool $statut,
+                        public int $IdLivre,
                                     )
     {
 
@@ -26,8 +22,6 @@ class ExemplairesDto
         $exemplaire = new Exemplaires();
         $exemplaire->setQuantite($this->quantite);
         $exemplaire->setEtat($this->etat);
-        $exemplaire->setLivres($this->livre);
-        $exemplaire->setEmprunt($this->emprunt);
         $exemplaire->setStatut($this->statut);
         $exemplaire->setIdLivre($this->IdLivre);
 
